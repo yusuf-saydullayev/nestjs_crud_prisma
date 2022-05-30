980d88e0f791057447f34e03ea72fdb3ebfd91e3
yusuf591-cpu <yusufsaydullayev@gmail.com>
2022-05-30 14:38:25 +0500

    format prettier

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-import { PrismaService } from '../prisma.service';
+import { PrismaService } from '../services/prisma.service';
 
 @Module({
   imports: [],
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
+import { PrismaService } from '../services/prisma.service';
 import { Product as ProductModel } from '@prisma/client';
 
 @Injectable()
