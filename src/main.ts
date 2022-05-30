11f15922875131592b9acdbd80fb3db66246091d
yusuf591-cpu <yusufsaydullayev@gmail.com>
2022-05-30 14:30:49 +0500

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => console.log('Server is running on port 3000'));
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+  const config = new DocumentBuilder()
+    .setTitle('Products example')
+    .setDescription('The products API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000, () => console.log('Server is running on port 3000'));
 }
 bootstrap();
